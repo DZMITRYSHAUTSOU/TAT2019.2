ec82a4ce8dfbfe44970e24540261584fc5cda5cf
DZMITRY <d.shautsov2@gmail.com>
2019-10-01 17:48:14 +0300

    Initial commit

---
+++ Aud_1/Aud_1/EntryPoint.cs
@@ -0,0 +1,35 @@
+﻿namespace CW_1
+{
+    /// <summary>
+    /// Main class
+    /// </summary>
+    class EntryPoint
+    {
+        /// <summary>
+        /// Entry Point
+        /// </summary>
+        static void Main()
+        {
+            Point A = new Point(0, 0);
+            Point B = new Point(2, 4);
+            Point C = new Point(4, 0);
+            Point A1 = new Point(1, 0);
+            Point B1 = new Point(3, 4);
+            Point C1 = new Point(5, 99);
+            bool result=false;
+            Triangle[] ArrayOfTriangles = new Triangle[3] {new Triangle(A, B, C), new Triangle(B, A, C), new Triangle(A1, B1, C)};
+            for (int i = 0; i < ArrayOfTriangles.Length; i++)
+            {
+                ArrayOfTriangles[i].DisplaySides();
+                for (int j = i+1; j < ArrayOfTriangles.Length; j++)
+                {
+                    if (ArrayOfTriangles[i].Equals(ArrayOfTriangles[j]))
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
