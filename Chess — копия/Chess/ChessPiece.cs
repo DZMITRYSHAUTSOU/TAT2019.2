5f21cbffca3f964ce9f386093f74fb7710bfbaf8
DZMITRY <d.shautsov2@gmail.com>
2019-11-13 17:55:23 +0300

    chess

---
+++ Chess — копия/Chess/ChessPiece.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class ChessPiece
+    {
+        public char HorizontalCoordinate { get; }
+        public int VerticalCoordinate { get; }
+        public ChessPiece()
+        {
+            HorizontalCoordinate = 'a';
+            VerticalCoordinate = 1;
+        }
+        public ChessPiece(char horizontalCoordinate, int verticalCoordinate)
+        {
+            if (ValidateCoordinates(horizontalCoordinate, verticalCoordinate))
+            {
+                HorizontalCoordinate = horizontalCoordinate;
+                VerticalCoordinate = verticalCoordinate;
+            }
+            else throw new InvalidChessCoordinatesException();
+        }
+        public override string ToString()
+        {
+            return "Position : " + HorizontalCoordinate + " " + VerticalCoordinate+"\n";
+        }
+        private bool ValidateCoordinates(char horizontalCoordinate, int verticalCoordinate)
+        {
+
+            return horizontalCoordinate.ToString().CompareTo("I") < 0
+                && Enumerable.Range(1, 8).Contains(verticalCoordinate);
+        }
+    }
+}
---
+++ Chess — копия/Chess/InvalidChessCoordinatesException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class InvalidChessCoordinatesException : Exception
+    {
+        public override string Message => "Wrong coordinates. Your coordinates must be in range 1-8 and a-h!";
+    }
+}
