f2b277dc2d9c5cacfa4c8edecd0a52187423d2c5
DZMITRY <d.shautsov2@gmail.com>
2019-11-19 00:21:26 +0300

    Initial commit, kinda works

---
+++ dev-3/dev-3/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev_3
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Translitor a = new Translitor("dabdabya");
+            Translitor b = new Translitor("дабдабя");
+            a.Translit();
+            b.Translit();
+            Console.WriteLine(a.ProcessedSentence);
+            Console.WriteLine(b.ProcessedSentence);
+        }
+    }
+}
---
+++ dev-3/dev-3/InvalidFormatException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev_3
+{
+    class InvalidFormatException : Exception
+    {
+        public override string Message => "Input string must be either only latin or only russian symbols.Ё is not supported";
+    }
+}
