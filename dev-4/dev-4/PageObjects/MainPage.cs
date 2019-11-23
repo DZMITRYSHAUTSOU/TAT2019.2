b4b84803c8fe52444afdc5e5f914f71d6e7ca896
DZMITRY <d.shautsov2@gmail.com>
2019-11-23 19:10:19 +0300

    Added new comments

--- dev-4/dev-4/PageObjects/MainPage.cs
+++ dev-4/dev-4/PageObjects/MainPage.cs
@@ -5,12 +5,18 @@ namespace dev_4
     class MainPage : PageObject
     {
         private IWebElement loginButton;
-
+        /// <summary>
+        /// Sets Login button and web driver fields
+        /// </summary>
+        /// <param name="webDriver">web driver used by page object</param>
         public MainPage(IWebDriver webDriver) : base(webDriver)
         {
             loginButton = webDriver.FindElement(By.CssSelector("[href*='https://r.mail.ru/n109322822']"));
         }
-
+        /// <summary>
+        /// Clicks login button element
+        /// </summary>
+        /// <returns>Login page</returns>
         public LoginPage GoToLoginPage()
         {
             loginButton.Click();
